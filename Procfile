18bdef705337f2796fd4a1a85376c2d52e4686a1
nanten-pi <nantenpizza@gmail.com>
2022-05-03 10:29:55 +0900

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+discordbot: python apitest2.py
---
+++ apitest2.py
@@ -0,0 +1,66 @@
+import discord
+import os
+import requests
+import json
+import os.path
+
+client = discord.Client()
+#気象庁から天気予報をいただくぜ
+def get_quote():
+    jma_url = "https://www.jma.go.jp/bosai/forecast/data/forecast/340000.json"
+    jma_json = requests.get(jma_url).json()
+
+    #取得したいデータを選ぶ
+    jma_place=jma_json[0]["publishingOffice"]
+    jma_timeget=jma_json[0]["reportDatetime"]
+    jma_date = jma_json[0]["timeSeries"][0]["timeDefines"][1]
+    jma_temp=jma_json[1]["tempAverage"]["areas"][0]["max"]
+    jma_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+    jma_weather = jma_weather.replace('　', '')
+    jma_return = ['明日の天気予報--南部--',jma_place,"予報取得時刻  "+jma_timeget,"予報時刻  "+jma_date,jma_weather,'最高気温  '+jma_temp+'℃','データーは気象庁より']
+    return('\n'.join(jma_return))
+#警報をいただくぜ
+#!/usr/bin/env python3
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+WEATHER_TRANS = json.load(open(base + "/transweather.json", "r",encoding="utf-8"))
+OFFICES_AREA_CODE = "340000" #地方エリアのコード
+CLASS_AREA_CODE = "3421200" #市町村区のコード
+AREA_URL = "https://www.jma.go.jp/bosai/common/const/area.json"
+
+url = "https://www.jma.go.jp/bosai/warning/data/warning/%s.json" % (OFFICES_AREA_CODE)
+def warnings():
+        area_data = requests.get(AREA_URL).json()
+        area = area_data["class20s"][CLASS_AREA_CODE]["name"]
+        warning_info = requests.get(url).json()
+
+        warning_codes = [warning["code"]
+                        for class_area in warning_info["areaTypes"][1]["areas"]
+                        if class_area["code"] == CLASS_AREA_CODE
+                        for warning in class_area["warnings"]
+                        if warning["status"] != "解除" and warning["status"] != "発表警報・注意報はなし"]
+        warning_texts = [WEATHER_TRANS["warninginfo"][code] for code in warning_codes]
+
+        return (area,warning_texts)
+#起動確認だよん
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+#コマンド実装
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$weather'):
+    quote = get_quote()
+    await message.channel.send(quote)
+  if message.content.startswith('$hei'):
+    await message.channel.send("やっほ")
+  if message.content.startswith('$aleat'):
+    warning = warnings()
+    await message.channel.send(warning)
+
+#pass
+client.run(os.getenv('TOKEN'))
